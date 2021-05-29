9b430a7665bf6e76b51401642902b5ef4134f4df
mahmut2sahin <mahmut2sahin@gmail.com>
2021-05-29 20:25:40 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace console_todo_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
